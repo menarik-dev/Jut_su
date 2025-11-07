b01a199e6e7a1cf7209ba5e48a553d06604b7dad
MarkI <menarik@ukr.net>
2025-11-07 19:21:26 +0200

    some changes

--- APITests/APIObjects/AuthObject.py
+++ APITests/APIObjects/AuthObject.py
@@ -1,6 +1,1 @@
-import httpx
-from typing import Dict, Any, Optional
-from dataclasses import asdict
 
-class AuthObject:
-    pass
--- conftest.py
+++ conftest.py
@@ -1,16 +1,17 @@
 import os
+
 import pytest
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from dotenv import load_dotenv
-from selenium.webdriver.chrome.webdriver import WebDriver
 
 from Utils.CaptchaByPass import CaptchaByPass
 from Utils.WaitUntil import WaitUntil
 
 load_dotenv()
 
-@pytest.fixture(scope='class')
+
+@pytest.fixture(scope="class")
 def driver_setup():
     options = Options()
     options.add_argument("--headless=new")
@@ -18,21 +19,26 @@ def driver_setup():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    options.add_argument(
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    )
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
     driver = webdriver.Chrome(options=options)
-    CaptchaByPass.PassCaptcha(driver=driver) # Pass captcha by javascript before page loading
+    CaptchaByPass.PassCaptcha(
+        driver=driver
+    )  # Pass captcha by javascript before page loading
     print(os.getenv("BASE_URL"))
-    driver.get(os.getenv("BASE_URL"))
+    driver.get(os.getenv("BASE_URL", ""))
 
-    yield driver # передает параметр тестам
+    yield driver  # передает параметр тестам
 
     # Same as TearDown
     if driver:
         driver.quit()
 
-@pytest.fixture(scope='function', autouse=True)
+
+@pytest.fixture(scope="function", autouse=True)
 def foreachtest(driver_setup):
     driver_setup.delete_all_cookies()
     driver_setup.get(os.getenv("BASE_URL"))
