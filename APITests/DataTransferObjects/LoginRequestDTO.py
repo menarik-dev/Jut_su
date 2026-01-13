bee9b25e66a26b70337c8ad506c391dfffabd8fd
Mancos <hsherlok02@gmail.com>
2026-01-13 15:07:25 +0200

    Delete APITests directory

--- APITests/DataTransferObjects/LoginRequestDTO.py
+++
@@ -1,9 +0,0 @@
-import requests
-from dataclasses import dataclass
-from typing import Optional
-
-@dataclass
-class LoginRequest:
-    login_name: Optional[str] = None
-    login_password: Optional[str] = None
-    login: Optional[str] = None
